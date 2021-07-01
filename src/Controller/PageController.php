2f4ef3c20022d5998914722b19afa18904185a80
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-01 13:37:15 +0200

    Methode contact avec Request

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,6 +23,21 @@ class PageController
      */
     public function legal(){
         return new Response('Mentions légales du super site de la mort qui tue');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(){
+        $request = Request::createFromGlobals();
+        if ($request->query->get('age')){
+            return new Response("Vous êtes autorisé à lire cette page");
+        }else{
+            return new Response("Vous n'êtes pas autorisé à lire cette page");
+        }
+
+
+
     }
 
 }
