b1f0059b51791b0369194db1d50ad2284f2c66b0
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-02 11:46:28 +0200

    Exo tableau article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,6 +10,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+public $articles = [
+1 => [
+"title" => "La vaccination c'est trop géniale",
+"content" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Libero ipsa ipsum sunt modi nesciunt officia, 
+                iste repellat quidem quae voluptatem quas animi, 
+                facilis voluptatibus quaerat eveniet, repellendus aut ullam delectus.",
+"id" => 1
+],
+2 => [
+"title" => "La vaccination c'est pas trop géniale",
+"content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+               Repudiandae asperiores officiis corporis modi minima odio temporibus! 
+               Dicta dolorum eaque minima, suscipit et voluptates cumque 
+               veritatis recusandae corrupti ipsa blanditiis minus.",
+"id" => 2
+],
+3 => [
+"title" => "Balkany c'est trop génial",
+"content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+               Porro eligendi asperiores doloremque aspernatur eum iure debitis commodi esse corporis tenetur? 
+               Ratione iste error veniam numquam. Esse totam sunt earum non?",
+"id" => 3
+],
+4 => [
+"title" => "Balkany c'est pas trop génial",
+"content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+               Voluptas, error amet suscipit dolore accusantium possimus tempore, 
+               ipsa deserunt voluptatem totam dolor explicabo placeat fugiat dolorum necessitatibus beatae neque velit adipisci.",
+"id" => 4
+]
+];
+
     /**
      * @Route("/articles", name="articleList")
      */
@@ -24,8 +57,9 @@ class ArticleController extends AbstractController
     */
    public function articleShow($id)
    {
-       //return new Response("Article n°".$id);
-       return $this->render('article.html.twig', ['id' => $id]);
+       $article = $this->articles[$id];
+       //return new Response("Titre de l'article : ".$article['title']);
+       return $this->render('article.html.twig', ['article' => $article]);
    }
 
 
