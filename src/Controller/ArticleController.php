70c9fbcde0d987396ab28071eaac4f94642ee31c
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-02 12:12:41 +0200

    Ajout HTML liste des articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,45 +10,48 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-public $articles = [
-1 => [
-"title" => "La vaccination c'est trop géniale",
-"content" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Libero ipsa ipsum sunt modi nesciunt officia, 
-                iste repellat quidem quae voluptatem quas animi, 
-                facilis voluptatibus quaerat eveniet, repellendus aut ullam delectus.",
-"id" => 1
-],
-2 => [
-"title" => "La vaccination c'est pas trop géniale",
-"content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-               Repudiandae asperiores officiis corporis modi minima odio temporibus! 
-               Dicta dolorum eaque minima, suscipit et voluptates cumque 
-               veritatis recusandae corrupti ipsa blanditiis minus.",
-"id" => 2
-],
-3 => [
-"title" => "Balkany c'est trop génial",
-"content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-               Porro eligendi asperiores doloremque aspernatur eum iure debitis commodi esse corporis tenetur? 
-               Ratione iste error veniam numquam. Esse totam sunt earum non?",
-"id" => 3
-],
-4 => [
-"title" => "Balkany c'est pas trop génial",
-"content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+
+    public $articles = [
+        1 => [
+        "title" => "La vaccination c'est trop géniale",
+        "content" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Libero ipsa ipsum sunt modi nesciunt officia, 
+                        iste repellat quidem quae voluptatem quas animi, 
+                        facilis voluptatibus quaerat eveniet, repellendus aut ullam delectus.",
+        "id" => 1
+        ],
+        2 => [
+            "title" => "La vaccination c'est pas trop géniale",
+            "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repudiandae asperiores officiis corporis modi minima odio temporibus! 
+                        Dicta dolorum eaque minima, suscipit et voluptates cumque 
+                        veritatis recusandae corrupti ipsa blanditiis minus.",
+            "id" => 2
+        ],
+        3 => [
+            "title" => "Balkany c'est trop génial",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Porro eligendi asperiores doloremque aspernatur eum iure debitis commodi esse corporis tenetur? 
+                        Ratione iste error veniam numquam. Esse totam sunt earum non?",
+            "id" => 3
+        ],
+        4 => [
+            "title" => "Balkany c'est pas trop génial",
+            "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                Voluptas, error amet suscipit dolore accusantium possimus tempore, 
                ipsa deserunt voluptatem totam dolor explicabo placeat fugiat dolorum necessitatibus beatae neque velit adipisci.",
-"id" => 4
-]
-];
+            "id" => 4
+            ]
+    ];
 
     /**
      * @Route("/articles", name="articleList")
      */
     public function articleList()
     {
-        return new Response("Liste d'articles du super site de la mort qui tue");
+        $list = $this->articles;
+        //return new Response(print_r($list));
+        return $this->render('articles.html.twig', ['list' => $list]);
     }
 
     // Système des wildcards
@@ -61,7 +64,5 @@ public $articles = [
        //return new Response("Titre de l'article : ".$article['title']);
        return $this->render('article.html.twig', ['article' => $article]);
    }
-
-
 
 }
